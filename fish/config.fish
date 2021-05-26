38e886f39f14c26bae416c0aee042e7aacfc9eff
Ben Dean-Kawamura <ben@mozilla.com>
2021-05-26 10:40:13 -0400

    Updating path handling

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,6 @@
-set -gx PATH ~/bin ~/.local/bin $PATH
+fish_add_path ~/bin
+fish_add_path ~/.local/bin
+fish_add_path ~/.config/scripts
 set -gx EDITOR nvim
 # Use gpg-agent for SSH auth
 set -gx  SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
