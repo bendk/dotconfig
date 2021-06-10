68c5a56a47261f62a6c9ecb4e36b180e7e36f5a5
Ben Dean-Kawamura <ben@mozilla.com>
2021-06-10 09:49:28 -0400

    Make floating help work on multiple opens

--- nvim/lua/floating_help.lua
+++ nvim/lua/floating_help.lua
@@ -2,7 +2,9 @@
 
 local map = require('map')
 
-local function on_load()
+local M = {}
+
+local function make_window_floating()
     -- Use a floating window
     local width = vim.api.nvim_get_option("columns")
     local height = vim.api.nvim_get_option("lines")
@@ -20,14 +22,17 @@ local function on_load()
     map.buf_map(0, 'n', 'q', '<cmd>:q<cr>')
 end
 
+function M.on_buf_enter()
+    if vim.opt.filetype:get() == "help" then
+	make_window_floating()
+    end
+end
+
 vim.api.nvim_exec([[
 augroup quickfix
 autocmd!
-autocmd! FileType qf :lua require("floating_help").on_load()
-autocmd! FileType help :lua require("floating_help").on_load()
+autocmd! BufEnter * :lua require("floating_help").on_buf_enter()
 augroup END
 ]], false)
 
-return {
-    on_load = on_load
-}
+return M
